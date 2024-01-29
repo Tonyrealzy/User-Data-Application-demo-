5a94ef7774aad151743217d97ad6f2a5e325b681
Smith <olaoluwasamsmith@gmail.com>
2024-01-29 09:06:33 +0100

    user-list

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user.model';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { CardComponent } from "../card/card.component";
 
 @Component({
@@ -10,10 +11,12 @@ import { CardComponent } from "../card/card.component";
     standalone: true,
     templateUrl: './homepage.component.html',
     styleUrl: './homepage.component.css',
-    imports: [CommonModule, RouterLink, CardComponent]
+    imports: [CommonModule, RouterLink, CardComponent, FormsModule]
 })
 export class HomepageComponent implements OnInit {
   users: User[] = [];
+  searchUsers: User[] = [];
+  searchTerm: string ='';
 
   constructor(private UserService: UserService, private router: Router){}
 
@@ -21,15 +24,22 @@ export class HomepageComponent implements OnInit {
     this.UserService.getAllUsers().subscribe(
       data => {
     this.users = data;
-  },
-  (error) => {
-    console.error('Error fetching users:', error);
+    },
+    (error) => {
+      console.error('Error fetching users:', error);
+    }
+    );
   }
-  );
-}
 
-viewUser(userId: number) : void {
-  this.router.navigate(['/viewUser', userId]);
-}
+  viewUser(userId: number) : void {
+    this.router.navigate(['/viewUser', userId]);
+  }
 
+  onSearchChange(searchTerm: string): void {
+    searchTerm = searchTerm.toLowerCase();
+    this.searchUsers = this.users.filter(user => 
+      (user && user.name && user.name.toLowerCase().includes(searchTerm)) || 
+      (user && user.nickname && user.nickname.toLowerCase().includes(searchTerm))
+    );
+  }
 }
